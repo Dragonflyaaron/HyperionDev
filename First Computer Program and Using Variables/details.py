fa00db7b9730eb93c3d3de83da39602f721e6195
Aaron Chester <152991421+Dragonflyaaron@users.noreply.github.com>
2023-12-05 14:59:10 +0000

    Add files via upload

---
+++ First Computer Program and Using Variables/details.py
@@ -0,0 +1,7 @@
+# create a variable to store the information and use = input to collect and store
+#print statement using the saved variables
+name = input ("What is your full name? ")
+age = input ("What is your age? ")
+number = input ("What is your house number? ")
+street = input ("what is your street name? " )
+print ("This is",name+".","They are",age,"years old and live at number ",number,"on", street+".")
---
+++ First Computer Program and Using Variables/hello_world.py
@@ -0,0 +1,10 @@
+#create a variable to store the users name & age i.e name, age
+    #use 'input =' to request and store the users name
+#use print and the variable 'name''age' to output the saved name & age
+#use print function to print statement "Hello World!"
+
+name = input ("What is your name? ")
+print ("Hi",name+"!")
+age = input  ("How old are you? ")
+print ("You are",age+" years old!")
+print ("Hello World!")
